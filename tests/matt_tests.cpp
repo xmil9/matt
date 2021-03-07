f8a3c652165ce12ce38679d8e6c0a1c9471e818a
Mike Lindner <mlindner9@yahoo.com>
2021-03-07 13:45:51 -0500

    Set up project for tests.

---
+++ tests/matt_tests.cpp
@@ -0,0 +1,13 @@
+//
+// Mar-2021, Michael Lindner
+// MIT license
+//
+#include <cstdlib>
+#include <iostream>
+
+
+int main()
+{
+   std::cout << "matt tests finished.\n";
+   return EXIT_SUCCESS;
+}
