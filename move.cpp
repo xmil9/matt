0aa722096073a44a69bb8bf65fa788ab8063222d
Mike Lindner <mlindner9@yahoo.com>
2021-03-20 12:29:16 -0400

    When notating capturing pawn moves add the departing file of the pawn.

--- move.cpp
+++ move.cpp
@@ -13,11 +13,17 @@ std::string notateMove(const Piece& piece, Square to, const Position& pos)
    // todo - check for ambiguities
    pos;
 
-   const bool isCapture = pos.isOccupiedBy(to, !piece.color());
-
    std::string notation = piece.notate();
+
+   const bool isCapture = pos.isOccupiedBy(to, !piece.color());
    if (isCapture)
+   {
+      // For capturing pawn moves the departing file is notated.
+      if (piece.isFigure(Figure::Pawn))
+         notation += piece.location().file();
       notation += "x";
+   }
+   
    notation += to.notate();
    return notation;
 }
