f7c37aa5e2a635d3e667d5adb5861a3102f1a9c5
Mike Lindner <mlindner9@yahoo.com>
2021-04-10 16:37:47 -0400

    Parallel execution of makeMove and tests.

--- matt.cpp
+++ matt.cpp
@@ -5,6 +5,7 @@
 #include "matt.h"
 #include "position.h"
 #include <algorithm>
+#include <execution>
 #include <iterator>
 
 
@@ -12,9 +13,76 @@ namespace
 {
 ///////////////////
 
-std::optional<Position> bestPosition(const std::vector<Position>& positions, Color side)
+template <typename T> class VectorSlice
 {
-   // Use best scoring next position depending on the piece's color.
+ public:
+   using const_iterator = typename std::vector<T>::const_iterator;
+
+   VectorSlice(const std::vector<T>& source, std::size_t start, std::size_t size);
+   VectorSlice(const std::vector<T>& source);
+
+   std::size_t size() const { return m_size; }
+   const const_iterator cbegin() const { return m_source->begin() + m_start; }
+   const const_iterator cend() const { return m_source->begin() + m_start + m_size; }
+   const const_iterator begin() const { return cbegin(); }
+   const const_iterator end() const { return cend(); }
+   const T& operator[](std::size_t idx) const { return (*m_source)[idx]; }
+
+ private:
+   const std::vector<T>* m_source = nullptr;
+   std::size_t m_start = 0;
+   std::size_t m_size = 0;
+};
+
+
+template <typename T>
+inline VectorSlice<T>::VectorSlice(const std::vector<T>& source, std::size_t start,
+                                   std::size_t size)
+: m_source{&source}, m_start{start}, m_size{size}
+{
+}
+
+
+template <typename T>
+inline VectorSlice<T>::VectorSlice(const std::vector<T>& source)
+: m_source{&source}, m_start{0}, m_size{source.size()}
+{
+}
+
+
+template <typename T>
+typename VectorSlice<T>::const_iterator begin(const VectorSlice<T>& slice)
+{
+   return slice.begin();
+}
+
+
+template <typename T>
+typename VectorSlice<T>::const_iterator end(const VectorSlice<T>& slice)
+{
+   return slice.end();
+}
+
+
+template <typename T>
+typename VectorSlice<T>::const_iterator cbegin(const VectorSlice<T>& slice)
+{
+   return slice.cbegin();
+}
+
+
+template <typename T>
+typename VectorSlice<T>::const_iterator cend(const VectorSlice<T>& slice)
+{
+   return slice.cend();
+}
+
+
+///////////////////
+
+std::optional<Position> bestPosition(const VectorSlice<Position>& positions, Color side)
+{
+   // Use best scoring position depending on the piece's color.
    const auto endPos = end(positions);
    auto bestPos = endPos;
    if (side == Color::White)
@@ -42,30 +110,65 @@ std::vector<Position> allMoves(const Position& pos, Color side)
 }
 
 
-std::optional<Position> makeMove_(const Position& pos, Color side, std::size_t plies)
+std::optional<Position> processMovesUnthreaded(const VectorSlice<Position>& moves,
+                                               Color side, std::size_t plies)
 {
-   const auto moves = allMoves(pos, side);
-
-   const std::vector<Position>* bestMoves = &moves;
-   std::vector<Position> bestNextMoves;
-   bestNextMoves.reserve(moves.size());
-
    if (plies > 1)
    {
+      // Look further ahead by, for each move, calculating all following moves of the
+      // opposite color.
+      std::vector<Position> bestNextMoves;
+      bestNextMoves.reserve(moves.size());
+
       for (const auto& move : moves)
       {
-         if (const auto nextMove = makeMove_(move, !side, plies - 1);
+         if (const auto nextMove =
+                processMovesUnthreaded(allMoves(move, side), !side, plies - 1);
              nextMove.has_value())
          {
             bestNextMoves.push_back(*nextMove);
          }
       }
 
-      bestMoves = &bestNextMoves;
+      return bestPosition(bestNextMoves, side);
    }
-
-   return bestPosition(*bestMoves, side);
+   else
+   {
+      // Final step. Simply find best of the given moves.
+      return bestPosition(moves, side);
+   }
 }
+
+
+std::optional<Position> processMoves(const VectorSlice<Position>& moves, Color side,
+                                     std::size_t plies)
+{
+   if (plies > 1)
+   {
+      // Look further ahead by, for each move, calculating all following moves of the
+      // opposite color.
+      std::mutex bestMovesMx;
+      std::vector<Position> bestNextMoves;
+      bestNextMoves.reserve(moves.size());
+
+      std::for_each(std::execution::par, begin(moves), end(moves), [&](const auto& move) {
+         if (const auto nextMove = processMoves(allMoves(move, side), !side, plies - 1);
+             nextMove.has_value())
+         {
+            std::lock_guard<std::mutex> lock(bestMovesMx);
+            bestNextMoves.push_back(*nextMove);
+         }
+      });
+
+      return bestPosition(bestNextMoves, side);
+   }
+   else
+   {
+      // Final step. Simply find best of the given moves.
+      return bestPosition(moves, side);
+   }
+}
+
 
 } // namespace
 
@@ -75,5 +178,5 @@ std::optional<Position> makeMove_(const Position& pos, Color side, std::size_t p
 std::optional<Position> makeMove(const Position& pos, Color side, std::size_t turns)
 {
    // Convert turns (one move of each player) to plies (one move of one player).
-   return makeMove_(pos, side, 2 * turns);
+   return processMoves(allMoves(pos, side), side, 2 * turns);
 }
--- tests/matt_tests.cpp
+++ tests/matt_tests.cpp
@@ -6,6 +6,7 @@
 #include "matt.h"
 #include "position.h"
 #include "test_util.h"
+#include "deps/essentutils/time_util.h"
 #include <iostream>
 #include <chrono>
 
@@ -14,13 +15,14 @@ namespace
 {
 ///////////////////
 
-void testMakeMove()
+void testMakeMove(const std::string& casePrefix, const Position& pos,
+                  std::size_t maxDepth, Color side)
 {
+   for (std::size_t i = 1; i <= maxDepth; ++i)
    {
-      const std::string caseLabel = "makeMove for depth 1";
+      const std::string caseLabel = casePrefix + std::to_string(i);
 
-      const Position pos{"Kwd3 wf4 Kbb2"};
-      const auto result = makeMove(pos, Color::White, 1);
+      const auto result = makeMove(pos, side, i);
 
       VERIFY(result.has_value(), caseLabel);
       if (result.has_value())
@@ -29,35 +31,38 @@ void testMakeMove()
          VERIFY(resPos != pos, caseLabel);
       }
    }
+}
+
+
+///////////////////
+
+void testMakeMoveForPositionA()
+{
+   const Position pos{"Kwd3 wf4 Kbb2"};
+
    {
-      const std::string caseLabel = "makeMove for depth 2";
-
-      const Position pos{"Kwd3 wf4 Kbb2"};
-      const auto result = makeMove(pos, Color::White, 2);
-
-      VERIFY(result.has_value(), caseLabel);
-      if (result.has_value())
-         VERIFY(*result != pos, caseLabel);
+      const std::string casePrefix = "Position A with white for depth ";
+      testMakeMove(casePrefix, pos, 4, Color::White);
    }
    {
-      const std::string caseLabel = "makeMove for depth 3";
+      const std::string casePrefix = "Position A with black for depth ";
+      testMakeMove(casePrefix, pos, 4, Color::Black);
+   }
+}
 
-      const Position pos{"Kwd3 wf4 Kbb2"};
-      const auto result = makeMove(pos, Color::White, 3);
 
-      VERIFY(result.has_value(), caseLabel);
-      if (result.has_value())
-         VERIFY(*result != pos, caseLabel);
+void testMakeMoveForPositionB()
+{
+   const Position pos{"Rwb1 Kwd1 Bwf1 Rwh1 wa2 wf2 wg2 wh2 Qbc3 we3 Nwf3 Bwg3 wd4 Kbe4 "
+                      "bd5 bg5 bc6 bh6 ba7 bb7 Kbe7 bf7 Bbg7 Rba8 Kbb8 Qwc8 Rbh8"};
+
+   {
+      const std::string casePrefix = "Position B with white for depth ";
+      testMakeMove(casePrefix, pos, 2, Color::White);
    }
    {
-      const std::string caseLabel = "makeMove for depth 4";
-
-      const Position pos{"Kwd3 wf4 Kbb2"};
-      const auto result = makeMove(pos, Color::White, 4);
-
-      VERIFY(result.has_value(), caseLabel);
-      if (result.has_value())
-         VERIFY(*result != pos, caseLabel);
+      const std::string casePrefix = "Position B with black for depth ";
+      testMakeMove(casePrefix, pos, 2, Color::Black);
    }
 }
 
@@ -68,5 +73,6 @@ void testMakeMove()
 
 void testMatt()
 {
-   testMakeMove();
+   testMakeMoveForPositionA();
+   testMakeMoveForPositionB();
 }
